48095b523d31aab2c86609f23657bd41a4000af3
DHARANI P <119476559+dhar127@users.noreply.github.com>
2025-06-12 20:58:38 +0530

    rectified errors

--- canteen-system/src/App.js
+++ canteen-system/src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RoleSelectionPage from "./components/RoleSelectionPage";
-<<<<<<< HEAD
+
 import LoginPage from "./components/LoginPage";
 import AdminRoutes from "./modules/admin/routes";
 import CanteenRoutes from "./modules/canteen/routes";
-=======
 //import LoginPage from "./components/LoginPage";
-import AdminRoutes from "./modules/admin/AdminDashboard";
-import CanteenRoutes from "./modules/canteen/CanteenRequestForm";
->>>>>>> f17de7e163b8f42fb569fa30cab8b7b1fab02b44
+
 import CustomerRoutes from "./modules/customer/routes";
 
 function App() {
